b35417c2e5a58cbf3a4e703ac9ad4d26be13963e
Wei Hung <wayne930242@gmail.com>
2022-08-14 18:37:55 +0800

    Modify home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/fate-condensed/intro">
+              命運 SRD 全文
           </Link>
         </div>
       </div>
